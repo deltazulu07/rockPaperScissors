6bf842b3c4c3c3aa00fcde8a0072a99d6cacbad2
deltazulu07 <133428143+deltazulu07@users.noreply.github.com>
2023-09-19 18:13:35 -0700

    Rewrote conditionals in switch statements

--- javascript.js
+++ javascript.js
@@ -5,32 +5,32 @@ function getComputerChoice() {
     const randomDecimal = Math.random();
 
     //if decimal [0, 1/3) "Rock", [1/3, 2/3) "Paper", [2/3, 1) "Scissors"
-    if (randomDecimal >=0 && randomDecimal < 1/3) {
-        return "Rock";
-    }
-    else if (randomDecimal >= 1/3 && randomDecimal < 2/3) {
-        return "Paper";
-    }
-    else {
-        return "Scissors";
+    switch (true) {
+        case  (randomDecimal >=0 && randomDecimal < 1/3):
+            return "Rock";
+        case  (randomDecimal >= 1/3 && randomDecimal < 2/3):
+            return "Paper";
+        default:
+            return "Scissors";
     }
 }
 
 function testGetComputerChoice(numOfLoops) {
     //test the function is truly random
 
-    //run the function 100 times, and count the occurrence of the 3 possible outcomes
+    //run the function numOfLoops times, and count the % of all 3 outcomes
     let countPaper = 0, countRock = 0, countScissors = 0;
+    
     for (let i = 0; i < numOfLoops; i++) {
-        let result = getComputerChoice();
-        if (result === "Rock") {
-            countRock += 1;
-        }
-        else if (result === "Paper") {
-            countPaper += 1;
-        }
-        else {
-            countScissors += 1;
+        switch (getComputerChoice()) {
+            case "Rock":
+                countRock += 1;
+                break;
+            case "Paper":
+                countPaper += 1;
+                break;
+            default:
+                countScissors += 1;
         }
     }
     console.log("Paper % of time: " + countPaper / numOfLoops);
@@ -38,4 +38,4 @@ function testGetComputerChoice(numOfLoops) {
     console.log("Scissors % of time: " + countScissors /numOfLoops);
 }
 
-testGetComputerChoice(1000);
+testGetComputerChoice(10000);
